ed5f8582f93959034b4a95290515695e2f534034
milan090 <milanmuhammed1@gmail.com>
2020-06-19 12:59:04 +0530

    Updates

---
+++ precache-manifest.9a071e5b67ed68f72c8f4bb8a90042b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ac01d5cb2041cf0880f991b4a1592c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "47e1933332c207c8649e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a94055d660a60b6fafe2",
+    "url": "/robofriends/static/css/main.ab4432cf.chunk.css"
+  },
+  {
+    "revision": "47e1933332c207c8649e",
+    "url": "/robofriends/static/js/2.58523ca3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.58523ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94055d660a60b6fafe2",
+    "url": "/robofriends/static/js/main.0282f294.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
